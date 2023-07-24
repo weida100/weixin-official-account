32299865fcc58d5d0624253d9e4f0e8739eaaecb
Weida <sgenmi@gmail.com>
2023-07-24 22:29:05 +0800

    公众号application

--- src/Application.php
+++ src/Application.php
@@ -18,58 +18,7 @@ use Weida\WeixinCore\Encryptor;
 
 class Application extends AbstractApplication
 {
-    protected ?ResponseInterface $response=null;
+    protected string $appType='officialAccount';
 
-    /**
-     * @return AccountInterface
-     * @author Sgenmi
-     */
-    public function getAccount(): AccountInterface
-    {
-        if (!$this->account){
-            $this->account = new Account(
-                $this->config->get('app_id'),
-                $this->config->get('secret'),
-                $this->config->get('token'),
-                $this->config->get('aes_key'),
-            );
-        }
-        return $this->account;
-    }
-
-    /**
-     * @return ResponseInterface
-     * @author Sgenmi
-     */
-    public function getResponse():ResponseInterface
-    {
-        if(empty($this->response)){
-            $this->response = new Response(
-                $this->getRequest(),
-                $this->getEncryptor()
-            );
-        }
-        return $this->response;
-    }
-    public function getServer():Response{
-        return $this->getResponse();
-    }
-
-    /**
-     * @return EncryptorInterface
-     * @author Sgenmi
-     */
-    public function getEncryptor(): EncryptorInterface
-    {
-        if(empty($this->encryptor)){
-            $this->encryptor = new Encryptor(
-                $this->getAccount()->getAppId(),
-                $this->getAccount()->getToken(),
-                $this->getAccount()->getAesKey(),
-                $this->getAccount()->getAppId()
-            );
-        }
-        return $this->encryptor;
-    }
 
 }
