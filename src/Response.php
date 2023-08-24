8700691cc11e12dc78f9bf4a4b7ed85a1176830f
Weida <sgenmi@gmail.com>
2023-08-25 00:12:53 +0800

    独立response

---
+++ src/Response.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+/**
+ * Author: Weida
+ * Date: 2023/8/24 22:23
+ * Email: sgenmi@gmail.com
+ */
+
+namespace Weida\WeixinOfficialAccount;
+
+use Psr\Http\Message\ResponseInterface;
+use Weida\WeixinCore\Contract\MessageInterface;
+use Weida\WeixinCore\AbstractResponse;
+use Weida\WeixinOfficialAccount\Message\Message;
+class Response extends AbstractResponse
+{
+    /**
+     * @return ResponseInterface
+     * @author Weida
+     */
+    public function serve():ResponseInterface{
+        if (!empty($this->params['echostr'])) {
+            $this->withBody($this->params['echostr']);
+            return $this;
+        }
+        $message = $this->getDecryptedMessage();
+        $response = $this->middleware->handler($this,$message);
+        if(empty($response)){
+            return $this;
+        }
+        if(is_string($response) || is_numeric($response)){
+            $response = Message::Text((string)$response);
+        }elseif (is_array($response)){
+            if(!empty($response['msgtype']) || !empty($response['MsgType'])){
+                $response = new Message($response);
+            }
+        }
+        if ($response instanceof MessageInterface){
+            $this->withHeader('Content-Type', 'application/xml;charset=utf-8');
+            if(!($response instanceof Message)){
+                $response = new Message($response->getAttributes());
+            }
+            $content = $response->toXmlReply($message,$this->encryptor);
+            $this->withBody($content);
+        }
+        return $this;
+    }
+
+}
